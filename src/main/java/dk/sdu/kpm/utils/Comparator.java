bc115d53f39c665e236a41e9284274c891997519
Anne Hartebrodt <anne.hartebrodt@tum.de>
2019-10-07 21:57:13 +0200

    pom

--- src/main/java/dk/sdu/kpm/utils/Comparator.java
+++ src/main/java/dk/sdu/kpm/utils/Comparator.java
@@ -1,10 +1,8 @@
 package dk.sdu.kpm.utils;
 
-/**
- * Comparators for P-value cutoff (when submitting a non binary matrix)
- */
+
 public enum Comparator {
-    GT, // greater than
-    LET // less or equal than
+    GT,
+    LET
 
 }
